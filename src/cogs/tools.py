c1c121f5ad19ed8e1cf4840126b6a1aa0c84dd11
Felix | onlix <neostyxde@gmail.com>
2021-08-26 22:08:36 +0200

    i dk

--- src/cogs/tools.py
+++ src/cogs/tools.py
@@ -10,21 +10,29 @@ class Tools(commands.Cog):
         self.client = client
 
     @commands.command(help='🔧An online counter! Add "server" to the command to only count your server.')
-    async def online(self, ctx, mode='all servers'):
+    async def online(self, ctx, mode='servers'):
+        mode = mode.lower()
+
         if mode == 'server':
-            minimum = 
-            maximum =
+            minimum = len([m for m in ctx.guild.members if (str(m.status) == 'online' and (not m.bot))])
+            maximum = len([m for m in ctx.guild.members if (not m.bot)])
+
+        elif mode == 'bots':
+            minimum = len([m for m in ctx.guild.members if (str(m.status) == 'online' and (m.bot))])
+            maximum = len([m for m in ctx.guild.members if (m.bot)])
+
         else:
-            minimum = 
-            maximum = 
+            pass
+            # minimum = len([m for m in set(self.client.get_all_members()) if (str(m.status) == 'online' and (m.bot))])
+            # maximum = len([m for m in set(self.client.get_all_members()) if (m.bot)])
         
         embed = discord.Embed(
-            title=f'Online Percentage for mode "{mode.title()}"',
-            description=f'> {minimum}/{maximum} (**{minimum/maximum*100}%**)',
-            color=values.color())
-        await ctx.send(embed=embed)
+            title=f'Online Percentage for {"Bots in " if mode == "bots" else ""}{"this Server" if mode != "servers" else "all Servers I have access to"}',
+            description=f'> {minimum}/{maximum} (**{round(minimum/maximum*100)}%**)',
+            color=values.color()
+        ).set_footer(text=f'Bots are {"not" if mode != "bots" else "exclusively"} counted')
 
-    @commands.command()
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Tools(client))
